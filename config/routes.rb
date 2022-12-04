b02b3ca004b2805b7d02670180e317bd6dac2c3d
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-04 18:59:56 +0300

    Modify file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'fallback/index'
   resources :users, only: [:index]
-  # root to: 'users#index'
   get '*path',
-      to: 'fallback#index',
-      constraints: ->(req) { !req.xhr? && req.format.html? }
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
+  
+  root to: 'users#index'
 end
