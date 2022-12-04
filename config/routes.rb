4d12f30cfb6652f2e1429a6f8242cd5a6b7c5d77
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-04 18:30:07 +0300

    Create and configure fallback controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'fallback/index'
   resources :users, only: [:index]
-  root to: 'users#index'
+  # root to: 'users#index'
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
