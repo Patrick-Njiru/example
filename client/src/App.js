39d65ce8a00a5c3da21376d5f9f84431ae96205d
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-12-04 16:31:21 +0300

    Modify file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [errors, setErrors] = useState([])
+
+  useEffect(() => {
+    fetch('/users').then(res => res.json()).then(setUsers)
+    //   if (res.ok) {
+    //     res.json().then(setUsers)
+    //   } else {
+    //     res.json().then(message => setErrors(message.error))
+    //   }
+    // })
+  }, [])
+
+  console.log(users);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +32,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> <br />
+        <code>My code</code>
+        <ol>
+          {users.map( user => <li key={user.id}>{user.name}</li>)}
+        </ol>
+
       </header>
     </div>
   );
